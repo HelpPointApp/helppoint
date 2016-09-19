52c01dedcaf70d2addad46024228a6ce47365c5a
Stanley Sathler <stanleysathlerpinto@gmail.com>
2016-09-18 23:11:09 -0300

    Agora, o mapa foca automaticamente na posição do usuário

--- application/app/src/main/java/com/example/reubert/appcadeirantes/manager/GPSManager.java
+++ application/app/src/main/java/com/example/reubert/appcadeirantes/manager/GPSManager.java
@@ -1,15 +1,18 @@
 package com.example.reubert.appcadeirantes.manager;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 
 public class GPSManager {
 
     private static GPSManager instance;
-
     private Context context;
     private final DialogInterface.OnClickListener activatePositiveOptionHandler = new ActivatePositiveOptionHandler();
 
@@ -22,6 +25,9 @@ public class GPSManager {
         return instance;
     }
 
+    public static boolean isPermissionGranted(Context context){
+        return ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
 
     public void enableLocation(){
         LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
@@ -30,8 +36,7 @@ public class GPSManager {
         }
     }
 
-
-    private void requireLocationEnabled(){
+    private void requireLocationEnabled() {
         final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
         alertDialogBuilder.setMessage("Para se tornar um herói, o GPS precisa estar ativo.");
         alertDialogBuilder.setCancelable(false);
@@ -39,6 +44,21 @@ public class GPSManager {
         alertDialogBuilder.create().show();
     }
 
+    /**
+     * @ToDo: create a model LoggedUser and move this method to that
+     * @ToDo: following Clean Code, null should never be returned. We'll implement exceptions later.
+     */
+    @SuppressWarnings("MissingPermission")
+    public Location getUserLocation(){
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        Location location = null;
+
+        if(GPSManager.isPermissionGranted(context)) {
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+        return location;
+    }
 
     private class ActivatePositiveOptionHandler implements DialogInterface.OnClickListener {
         public void onClick(DialogInterface dialog, int id) {
--- application/app/src/main/java/com/example/reubert/appcadeirantes/view/MapsActivity.java
+++ application/app/src/main/java/com/example/reubert/appcadeirantes/view/MapsActivity.java
@@ -1,15 +1,16 @@
 package com.example.reubert.appcadeirantes.view;
 
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.example.reubert.appcadeirantes.R;
 import com.example.reubert.appcadeirantes.manager.GPSManager;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 
 /**********************************************************************
@@ -21,7 +22,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap googleMap;
     private GPSManager gpsManager;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,25 +31,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
 
         configureGPS();
         configureMap();
+        focusOnCurrentUserPosition();
     }
-
 
     private void configureGPS(){
         gpsManager = GPSManager.getInstance(this);
         gpsManager.enableLocation();
     }
 
-
+    @SuppressWarnings("MissingPermission")
     private void configureMap(){
-        try {
+        if(GPSManager.isPermissionGranted(this)){
             googleMap.setMyLocationEnabled(true);
-        } catch(SecurityException exception){}
+        }
+    }
+
+    private void focusOnCurrentUserPosition(){
+        Location userLocation = gpsManager.getUserLocation();
+        final double latitude = userLocation.getLatitude();
+        final double longitude = userLocation.getLongitude();
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 17));
     }
 }
