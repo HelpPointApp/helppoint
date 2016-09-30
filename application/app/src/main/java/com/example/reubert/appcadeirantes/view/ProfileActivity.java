d7d14d524da781fb8b627b13e387a4218cdb82a3
reubert22 <reubertfernandes@gmail.com>
2016-09-30 15:46:48 -0300

    need to implement activity and connections w/ db

---
+++ application/app/src/main/java/com/example/reubert/appcadeirantes/view/ProfileActivity.java
@@ -0,0 +1,40 @@
+package com.example.reubert.appcadeirantes.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.reubert.appcadeirantes.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    public TextView txtEmail;
+    public TextView txtNascimento;
+    public TextView txtCpf;
+
+    public Button btnVoltar;
+
+    public ImageView imgPerfil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        loadAllViewElements();
+
+    }
+
+    public void loadAllViewElements(){
+        txtEmail = (TextView) findViewById(R.id.txtProfileEmail);
+        txtNascimento = (TextView) findViewById(R.id.txtProfileNascimento);
+        txtCpf        = (TextView) findViewById(R.id.txtProfileCpf);
+
+        btnVoltar     = (Button) findViewById(R.id.btnProfileVoltar);
+
+        imgPerfil     = (ImageView) findViewById(R.id.imgProfile);
+    }
+}
---
+++ application/connectionMysql/README.txt
@@ -0,0 +1,3 @@
+Modelo de teste para conexão com banco de dados MySQL,
+é preciso modificar em ambos arquivos, parâmetros para
+funcionar.
