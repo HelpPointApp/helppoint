e2590f746c010ad4f968de83e4be9d4c81d45da7
Aron Castro <aron.castro.coelho@gmail.com>
2016-11-13 20:37:01 -0200

    Added window of help and actions and states for model Help

--- application/app/src/main/java/com/example/reubert/appcadeirantes/model/Avaliation.java
+++ application/app/src/main/java/com/example/reubert/appcadeirantes/model/Avaliation.java
@@ -2,9 +2,18 @@ package com.example.reubert.appcadeirantes.model;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("Avaliation")
 public class Avaliation extends ParseObject {
+
+    public void setUserHelp(ParseUser user){
+        put("userHelp", user);
+    }
+
+    public ParseUser getUserHelp(){
+        return (ParseUser) get("userHelp");
+    }
 
     public void setHelp(Help help){
         put("help", help);
